129ddabe56a86f979baa35abb050699cb4612688
MrCrabsss <506992469@qq.com>
2024-05-28 04:05:59 +0000

    first commit

---
+++ config.py
@@ -0,0 +1,18 @@
+# dataset name 
+dataset = 'ml-1m'
+
+# model name 
+model = 'NeuMF-end'
+assert model in ['MLP', 'GMF', 'NeuMF-end']
+
+# paths
+main_path = '/data/lab/NCF_example1/Data/'
+
+train_rating = main_path + '{}.train.rating'.format(dataset)
+test_rating = main_path + '{}.test.rating'.format(dataset)
+test_negative = main_path + '{}.test.negative'.format(dataset)
+
+model_path = './models/'
+GMF_model_path = model_path + 'GMF.pth'
+MLP_model_path = model_path + 'MLP.pth'
+NeuMF_model_path = model_path + 'NeuMF.pth'
